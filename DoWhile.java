866af7ed05e2836a7b22bde882f34b7545e6f6b8
Davi Fernando Rosa <davir0514@gmail.com>
2021-05-21 18:59:09 -0300

    Arquivo

---
+++ DoWhile.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Davi
+ */
+public class DoWhile {
+    
+    public static void main(String[]args){
+    
+         int valor = 0;
+         do{
+            System.out.println("O numero é: " + valor);
+            valor++;
+           }while (valor < 5);
+    
+    }
+
+
+}
